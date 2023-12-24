917a6e594ad69de0581da0ea8571ebeee6e19665
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2023-12-24 21:09:07 +0600

    final

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -48,6 +48,64 @@ def get_one_category(category):
             return dumps(products_list)
         else:
             return jsonify({"error": "No products found in this category"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/api/orders/all', methods=['GET'])
+def get_all_orders():
+    try:
+        orders_collection = mongo.db['orders']
+        orders = orders_collection.find()
+        orders_list = list(orders)
+        if orders_list:
+            return dumps(orders_list)
+        else:
+            return jsonify({"error": "No orders found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/api/carts', methods=['POST'])
+def create_new_cart():
+    try:
+        carts_collection = mongo.db['carts']
+        cart_data = request.json
+        result = carts_collection.insert_one(cart_data)
+        return jsonify({"success": True, "id": str(result.inserted_id)}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
+
+@app.route('/api/carts/items', methods=['GET'])
+def get_cart_items():
+    try:
+        carts_collection = mongo.db['carts']
+        carts = carts_collection.find()
+        carts_list = list(carts)
+        if carts_list:
+            return dumps(carts_list)
+        else:
+            return jsonify({"error": "No orders found"}), 404
+        return jsonify({"success": True, "id": str(result.inserted_id)}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/api/users/<id>', methods=['GET'])
+def get_user_data(id):
+    try:
+        users_collection = mongo.db['users']
+        users = users_collection.find({"id": ObjectId(id)})
+        users_list = list(users)
+        if users_list:
+            return dumps(users_list)
+        else:
+            return jsonify({"error": "No user found"}), 404
+        return jsonify({"success": True, "id": str(result.inserted_id)}), 201
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
